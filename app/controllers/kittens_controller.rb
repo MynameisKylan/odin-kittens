ced411f90e8bec0ad3a2b5ae91c471c01452a82a
Kylan Wong <mynameiskylan@gmail.com>
2020-10-16 12:40:48 -0700

    add new/edit form

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,29 +1,53 @@
 class KittensController < ApplicationController
   def index
-    
+    @kittens = Kitten.all
   end
 
   def show
-
+    @kitten = Kitten.find(params[:id])
   end
 
   def new
-
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
 
+    if @kitten.save
+      flash.notice = 'New kitten created'
+      redirect_to @kitten
+    else
+      flash.now.notice = 'You can\'t have a kitten without all this info'
+      render :new
+    end
   end
 
   def edit
-  
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
-
+    if @kitten.update(kitten_params)
+      flash.notice = 'Kitten has been updated'
+      redirect_to @kitten
+    else
+      flash.now.notice = 'You can\'t have a kitten without all this info'
+      render :edit
+    end
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
 
+    @kitten.destroy
+    flash.notice = 'You have destroyed that kitten. You monster.'
+    redirect_to kittens_path
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
